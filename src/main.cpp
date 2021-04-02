26d44a6db5baf770cb84f3c2dffad8a02dc73712
Lee Witek <5556121+0xcafed00d@users.noreply.github.com>
2021-04-02 17:26:43 +0100

    testing keypad

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,28 @@ using namespace pimoroni;
 
 PicoRGBKeypad pico_keypad;
 
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+
 int main() {
-	bi_decl(bi_program_description("template raspberry pi pico c++ project"));
+	bi_decl(bi_program_description("pimoroni rgb keypad project"));
 
 	stdio_init_all();
 
-	const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-
 	gpio_init(LED_PIN);
 	gpio_set_dir(LED_PIN, GPIO_OUT);
+	pico_keypad.init();
 
 	uint32_t count = 1;
+
+	pico_keypad.illuminate(0, 255, 255, 255);
 
 	while (true) {
 		gpio_put(LED_PIN, count & 1);
 		printf("%d\n", count);
 		sleep_ms(100);
 		count++;
+
+		pico_keypad.set_brightness((count & 0xf) / 16.0f);
+		pico_keypad.update();
 	}
 }
